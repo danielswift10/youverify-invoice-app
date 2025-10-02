40baaaa7912565318d6d72efb0025ab50dbb1097
danielswift@10 <ojeyomi.daniel@gmail.com>
2025-10-02 18:33:05 +0100

    fix: linked modal properly

--- src/(dashboard)/invoice/all-invoices/AllInovices.tsx
+++ src/(dashboard)/invoice/all-invoices/AllInovices.tsx
@@ -1,12 +1,16 @@
+import { useStore } from "@nanostores/react";
+import InvoiceModal from "../components/InvoiceModal";
 import AllInvoicesHeader from "./components/AllInvoicesHeader";
 import AllInvoicesTable from "./components/AllInvoicesTable";
+import { $modalType } from "../../../store";
 
 export default function AllInovices() {
+  const modalType = useStore($modalType);
   return (
-    <div className='mt-52'>
-        <AllInvoicesHeader/>
-        <AllInvoicesTable/>
-
+    <div className="mt-52">
+      <AllInvoicesHeader />
+      <AllInvoicesTable />
+      <InvoiceModal isOpen={modalType === "INVOICE_MODAL"} />
     </div>
-  )
+  );
 }
